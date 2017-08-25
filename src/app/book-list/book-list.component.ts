cdc7a4015bca66e11e873adc6a97608db88380d2
Nguyet Nguyen <nguyetnguyen1903@gmail.com>
2017-08-25 21:35:03 +0200

    BookList LifeCycle Hooks (onDestroy, ng-content)

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,23 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BookService} from '../shared/services/book.service';
 import {Book} from '../shared/interfaces/book';
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
   public books: Book[];
 
+  private bookSubscription: Subscription;
+
   constructor(private bookService: BookService) {
-    this.bookService.getBooks().subscribe(books => {
+  }
+
+  ngOnInit() {
+    this.bookSubscription = this.bookService.getBooks().subscribe(books => {
       this.books = books;
     });
   }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.bookSubscription.unsubscribe();
   }
-
 }
