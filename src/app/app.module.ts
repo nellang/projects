20719e4d391cfd4759b46e93cb09edd27508370d
Nguyet Nguyen <nguyetnguyen1903@gmail.com>
2017-08-25 22:11:17 +0200

    BookList Routing BookDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import {AppComponent} from './app.component';
 import {BookService} from './shared/services/book.service';
 import {BookListComponent} from './book-list/book-list.component';
 import {HttpClientModule} from '@angular/common/http';
+import {routing} from './app.routing';
+import {BookDetailComponent} from './book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    routing
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
   {path: 'books', component: BookListComponent},
-  {path: '', redirectTo: 'books', pathMatch: 'full'}
+  {path: 'books/:id', component: BookDetailComponent}
 ];
 
 export const routing = RouterModule.forRoot(routes);
